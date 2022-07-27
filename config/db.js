ae3f957d2369b4ab5477c378fc2f9ca773622a36
LINA KHEDIKAR <linakhedikar151@gmail.com>
2022-07-27 11:49:35 +0530

    mongodb connection changes

--- config/db.js
+++ config/db.js
@@ -1,31 +1,16 @@
 require('dotenv').config();
-// const mongoose = require("mongoose");
+
 // password cloud mongodb : jWow7jdwPiQDY9yG
-// function connectDB() {
-//     //Database connection
-//     mongoose.connect(process.env.MONGO_CONNECTION_URL,{ useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology:true, useFindAndModify:true});
-//     const connection = mongoose.connection;
-
-//     connection.once('open',()=>{
-//         console.log('Database connected.');
-//     }).catch(err =>{
-//         console.log('Connection failed.');
-//     })
-// }
-
-// module.exports = connectDB;
 
 const mongoose = require("mongoose");
 function connectDB() {
-mongoose.connect(process.env.MONGO_CONN, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Database Connected.");
-  })
-  .catch((e) => {
-    console.log("Connection Failed");
-  });
-}
+mongoose.connect(process.env.MONGO_CONNECTION_URL,{ useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology:true, useFindAndModify:true});
+    const connection = mongoose.connection;
+    connection.once('open', function connectDB() {
+      console.log('Database Connected');
+    })
+    .on('error', function connectDB (err) {
+      console.log(err);
+    });
+  }
 module.exports = connectDB;
